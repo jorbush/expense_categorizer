2ce6e9d0382afe0e23b1e11aaf20088c76afc179
Jordi <55429631+jorbush@users.noreply.github.com>
2024-05-26 19:18:54 +0200

    Improve categorizer and output

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ import pandas as pd
 file_path = 'expenses.xls'
 df = pd.read_excel(file_path, skiprows=8)
 
-print(f"Original Data:\n{df}")
-print(f"Columns:\n{df.columns}")
-
 def categorize_transactions(df):
     custom_categories = {
-        'Supermarket': ['mercadona', 'carrefour'],
-        'Entertainment': ['netflix', 'spotify'],
-        'Restaurants': ['mcdonalds', 'starbucks'],
+        'Supermarket': ['caprabo', 'mercadona', 'carrefour', 'lidl', 'la sirena', 'dia', 'bonpreu', 'bon area'],
+        'Entertainment': ['netflix', 'spotify', 'itunes'],
+        'Bar/Restaurants': ['bar', 'fornet', 'restaurant', 'pizzeria', 'mcdonalds', 'heladeria', 'ramen', 'sushi', 'l ovella'],
+        'Bizum': ['bizum'],
+        'Shopping': ['amazon', 'revolut', 'mini mercat'],
     }
 
     def assign_category(description):
         description_lower = description.lower()
-        print(description_lower)
+        print(f"    {description_lower}")
         for category, keywords in custom_categories.items():
             if any(keyword in description_lower for keyword in keywords):
                 return category
@@ -24,27 +23,30 @@ def categorize_transactions(df):
     df['Category'] = df['Concepto'].apply(assign_category)
     return df
 
+print("\nConcepts:")
 df = categorize_transactions(df)
 
 expenses = df[df['Importe'] < 0]
 income = df[df['Importe'] > 0]
 
-print("Categorized Expenses:")
-print(expenses.groupby('Category').sum())
+print("\nCategorized Expenses:")
+for category, group in expenses.groupby('Category'):
+    total = group['Importe'].sum()
+    print(f"    {category}: {total:.2f} EUR")
+total_expenses = expenses['Importe'].sum()
+print(f"Total Expenses: {total_expenses:.2f} EUR")
 
 print("\nCategorized Income:")
-print(income.groupby('Category').sum())
-
+for category, group in income.groupby('Category'):
+    total = group['Importe'].sum()
+    print(f"    {category}: {total:.2f} EUR")
 
 total_income = income['Importe'].sum()
-total_expenses = expenses['Importe'].sum()
-print(f"\nTotal Income: {total_income:.2f} EUR")
-print(f"Total Expenses: {total_expenses:.2f} EUR")
+print(f"Total Income: {total_income:.2f} EUR")
+
 total_savings = total_income + total_expenses
 print(f"\nTotal Savings: {total_savings:.2f} EUR")
 
-
-# Optional: Save the result to a new Excel file
 output_path = 'categorized_file.xlsx'
 df.to_excel(output_path, index=False)
 print(f"\nThe categorized file has been saved to: {output_path}")
